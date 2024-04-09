30f63e25794476343207552a739ee912fe5a77e2
VigneshKabilan <kabilanvignesh2002@gmail.com>
2024-04-09 22:06:12 +0530

    completed Graph component

---
+++ ClientApp/src/data/Data.js
@@ -0,0 +1,12 @@
+export const TimeLineChartData = [
+    {x: ['2023-11-02', '2024-01-01'], y: 'Work1', status: 'Completed'},
+    {x: ['2023-11-12', '2023-12-03'], y: 'Work2', status: 'Completed'},
+    {x: ['2023-12-15', '2024-01-15'], y: 'Work3', status: 'Completed'},
+    {x: ['2023-12-25', '2024-01-05'], y: 'Work4', status: 'Completed'},
+    {x: ['2024-01-04', '2024-01-31'], y: 'Work5', status: 'Completed'},
+    {x: ['2024-02-14', '2024-03-12'], y: 'Work6', status: 'Completed'},
+    {x: ['2024-02-28', '2024-03-30'], y: 'Work7', status: 'Delayed'},
+    {x: ['2024-03-05', '2024-04-03'], y: 'Work8', status: 'Completed'},
+    {x: ['2024-03-20', '2024-04-29'], y: 'Work9', status: 'Pending'},
+    {x: ['2024-04-01', '2024-05-12'], y: 'Work10', status: 'Pending'}
+];
--- ClientApp/src/screens/WorkPage.js
+++ ClientApp/src/screens/WorkPage.js
@@ -6,6 +6,9 @@ import "./screen css/WorkPage.css";
 import NavBar from '../component/NabBar';
 import TimeLineChart from '../component/TimeLineChart';
 
+//Data import
+import { TimeLineChartData } from '../data/Data';
+
 const WorkPage = () => {
     const [itemData, setItemData] = useState([]);
     const [loading, setLoading] = useState(false);
@@ -82,7 +85,31 @@ const WorkPage = () => {
              ): (
                 <div>
                     <NavBar />
-                    <TimeLineChart />
+                    <TimeLineChart 
+                        Data={ TimeLineChartData }
+                        backgroundColor={
+                            [
+                                'rgba(255, 26, 104, 0.7)',
+                                'rgba(54, 162, 235, 0.7)',
+                                'rgba(255, 206, 86, 0.7)',
+                                'rgba(75, 192, 192, 0.7)',
+                                'rgba(153, 102, 255, 0.7)',
+                                'rgba(255, 159, 64, 0.7)',
+                                'rgba(0, 0, 0, 0.7)'
+                            ]
+                        }
+                        borderColor={
+                            [
+                                'rgba(255, 26, 104, 1)',
+                                'rgba(54, 162, 235, 1)',
+                                'rgba(255, 206, 86, 1)',
+                                'rgba(75, 192, 192, 1)',
+                                'rgba(153, 102, 255, 1)',
+                                'rgba(255, 159, 64, 1)',
+                                'rgba(0, 0, 0, 1)'
+                            ]
+                        }
+                    />
                 </div>
             )}
         </>
