9124cdcb7fb46958dfd1313954fb985bf418393a
VigneshKabilan <kabilanvignesh2002@gmail.com>
2024-05-06 22:52:47 +0530

    changes

--- ClientApp/src/context/Protected.js
+++ ClientApp/src/context/Protected.js
@@ -1,17 +1,14 @@
-import React from "react";
-import { Navigate } from 'react-router-dom'
+import React, { useEffect } from "react";
+import { Navigate, useNavigate } from 'react-router-dom'
 import { UserAuth } from "./AuthContext";
 import useLocalStorage from "./useLocalStorage";
 
 const Protected = ({children}) => {
     const { user } = UserAuth();
-    const { getItem } = useLocalStorage('login');
-    const data = getItem();
 
     if (!user) {
         return <Navigate to='/' />;
     }
-
     return children;
 };
 
